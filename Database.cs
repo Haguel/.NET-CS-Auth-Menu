794eb4b56a3af5cde254645970b12e5d3a20d04f
GlebHal <hammanov.gleb@gmail.com>
2023-03-16 10:42:52 +0200

    Reworked User. Separated db to a separate file

---
+++ Database.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Database
+    {
+        private string jsonRoute = "../../data.json";
+
+        private bool IsDB_Existed()
+        {
+            try { LoadUsers(); }
+            catch { return false; }
+
+            return true;
+        }
+
+        public Database() 
+        {
+            if (!IsDB_Existed()) SaveUsers(new List<CompletedUser>());
+        }
+
+        public List<CompletedUser> LoadUsers()
+        {
+            string dbJson = File.ReadAllText(jsonRoute);
+
+            return JsonConvert.DeserializeObject<List<CompletedUser>>(dbJson);
+        }
+
+        public void SaveUsers(List<CompletedUser> users)
+        {
+            string dbJson = JsonConvert.SerializeObject(users);
+
+            File.WriteAllText(jsonRoute, dbJson);
+        }
+
+        public CompletedUser FindOne(string email)
+        {
+            List<CompletedUser> existedUsers = LoadUsers();
+
+            foreach (CompletedUser existedUser in existedUsers)
+            {
+                if (existedUser.email == email) return existedUser;
+            }
+
+            return null;
+        }
+
+        public void UpdateOne(CompletedUser existedUser, CompletedUser updatedUser)
+        {
+            try
+            {
+                if (FindOne(existedUser.email) == null)
+                {
+                    throw new Exception("UserModel, updateOne() error: User wasn't found.");
+                }
+
+                new UserModel(updatedUser).Save();
+
+                DeleteOne(existedUser);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+
+                Environment.Exit(0);
+            }
+
+        }
+
+        public void DeleteOne(CompletedUser existedUser)
+        {
+            try
+            {
+                if (FindOne(existedUser.email) == null)
+                {
+                    throw new Exception("UserModel, deleteOne() error: User wasn't found.");
+                }
+
+                List<CompletedUser> existedUsers = LoadUsers();
+
+                existedUsers.Remove(existedUser);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+
+                Environment.Exit(0);
+            }
+        }
+    }
+}
--- User.cs
+++ User.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using Newtonsoft.Json;
 
 namespace App
 {
-    internal class User
+    internal class BaseUser
     {
         public string login;
         public string email;
@@ -16,22 +14,22 @@ namespace App
         public string imageSrc;
     }
 
-    internal class UserJson
+    internal class CompletedUser
     {
         public string login { get; }
         public string email { get; }
         public string passwordHash { get; }
         public string imageSrc { get; }
 
-        public UserJson((string login, string email, string passwordHash, string imageSrc) data)
+        public CompletedUser(string login, string email, string passwordHash, string imageSrc)
         {
-            login = data.login;
-            email = data.email;
-            passwordHash = data.passwordHash;
-            imageSrc = data.imageSrc;
+            login = login;
+            email = email;
+            passwordHash = passwordHash;
+            imageSrc = imageSrc;
         }
 
-        public UserJson()
+        public CompletedUser()
         {
             login = "";
             email = "";
@@ -42,36 +40,13 @@ namespace App
 
     internal class UserModel
     {
-        private string dbJsonRoute = "../../data.json";
-
+        Database database;
         private (string value, bool isUnique, bool isRequired) login;
         private (string value, bool isUnique, bool isRequired) email;
         private (string value, bool isUnique, bool isRequired) passwordHash;
         private (string value, bool isUnique, bool isRequired) imageSrc;
 
-        private bool isDB_Existed()
-        {
-            try { loadDB(); }
-            catch { return false; }
-
-            return true;
-        }
-
-        private List<UserJson> loadDB()
-        {
-            string dbJson = File.ReadAllText(dbJsonRoute);
-
-            return JsonConvert.DeserializeObject<List<UserJson>>(dbJson);
-        }
-
-        private void saveDB(List<UserJson> users)
-        {
-            string dbJson = JsonConvert.SerializeObject(users);
-
-            File.WriteAllText(dbJsonRoute, dbJson);
-        }
-
-        private void checkRequireness()
+        private void CheckRequireness()
         {
             if (login.isRequired && login.value == null) throw new Exception("UserModel, save() error: Login is required.");
             if (email.isRequired && email.value == null) throw new Exception("UserModel, save() error: Email is required.");
@@ -79,11 +54,11 @@ namespace App
             if (imageSrc.isRequired && imageSrc.value == null) throw new Exception("UserModel, save() error: Image source is required.");
         }
 
-        private void checkUniqueness()
+        private void CheckUniqueness()
         {
-            List<UserJson> existedUsers = loadDB();
+            List<CompletedUser> existedUsers = database.LoadUsers();
 
-            foreach (UserJson existedUser in existedUsers)
+            foreach (CompletedUser existedUser in existedUsers)
             {
                 if (login.isUnique)
                 {
@@ -107,87 +82,32 @@ namespace App
             }
         }
 
-        public UserModel(string login, string email, string passwordHash, string imageSrc)
+        public UserModel(CompletedUser userCompleted)
         {
-            this.login = (login, false, true);
-            this.email = (email, true, true);
-            this.passwordHash = (passwordHash, false, true);
-            this.imageSrc = (.imageSrc, false, false);
+            this.login = (userCompleted.login, false, true);
+            this.email = (userCompleted.email, true, true);
+            this.passwordHash = (userCompleted.passwordHash, false, true);
+            this.imageSrc = (userCompleted.imageSrc, false, false);
 
-            if(!isDB_Existed()) saveDB(new List<UserJson>());
+            database = new Database();
         }
 
-        public void save()
+        public void Save()
         {
             try
             {
-                checkRequireness();
-                checkUniqueness();
+                CheckRequireness();
+                CheckUniqueness();
 
-                UserJson newUser = new UserJson((login.value, email.value, passwordHash.value, imageSrc.value));
+                CompletedUser newUser = new CompletedUser(login.value, email.value, passwordHash.value, imageSrc.value);
 
-                List<UserJson> existedUsers = loadDB();
+                List<CompletedUser> existedUsers = database.LoadUsers();
 
                 existedUsers.Add(newUser);
 
-                saveDB(existedUsers);
+                database.SaveUsers(existedUsers);
             }
             catch (Exception err)
-            {
-                Console.WriteLine(err);
-
-                Environment.Exit(0);
-            }
-        }
-
-        public UserJson findOne(string email)
-        {
-            List<UserJson> existedUsers = loadDB();
-
-            foreach (UserJson existedUser in existedUsers)
-            {
-                if (existedUser.email == email) return existedUser;
-            }
-
-            return null;
-        }
-
-        public void updateOne(UserJson existedUser, UserJson updatedUser)
-        {
-            try
-            {
-                if (findOne(existedUser.email) == null)
-                {
-                    throw new Exception("UserModel, updateOne() error: User wasn't found.");
-                }
-
-                new UserModel(updatedUser.login, updatedUser.email, updatedUser.passwordHash, updatedUser.imageSrc).save();
-
-                deleteOne(existedUser);
-            }
-            catch(Exception err) 
-            {
-                Console.WriteLine(err);
-
-                Environment.Exit(0);
-            }
-            
-        }
-
-        public void deleteOne(UserJson existedUser) 
-        {
-            try
-            {
-                if (findOne(existedUser) == null)
-                {
-                    throw new Exception("UserModel, deleteOne() error: User wasn't found.");
-                }
-
-                List<UserJson> existedUsers = loadDB();
-
-                existedUsers.Remove(existedUser);
-            } 
-            catch(Exception err) 
             {
                 Console.WriteLine(err);
 
