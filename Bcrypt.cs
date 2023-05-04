9800c64d7d1e34db21386318e31a7a6be2ffe8ad
GlebHal <hammanov.gleb@gmail.com>
2023-05-04 11:03:02 +0300

    Fixed bugs, added some adjustments

---
+++ Bcrypt.cs
@@ -0,0 +1,9 @@
+﻿namespace App
+{
+    internal class Bcrypt
+    {
+        public string GenSalt(int length) { return BCrypt.Net.BCrypt.GenerateSalt(length); }
+        public string Hash(string value, string salt) { return BCrypt.Net.BCrypt.HashPassword(value, salt); }
+        public bool Verify(string value, string hash) { return BCrypt.Net.BCrypt.Verify(value, hash); }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using App;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Output();
+        }
+    }
+}
